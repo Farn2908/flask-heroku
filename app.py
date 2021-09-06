c18ebf8f68a6c937415ad37ad9f9458a656c57aa
Farn2908 <88994711+Farn2908@users.noreply.github.com>
2021-09-06 09:25:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ data = [
     ]
 
 @app.route('/')
-def hello
-    return "Hello ฟาน สีงวน เลขที่29 ห้องม.4/8"
+def hello():
+    return "Hello ฟาน สีงวน เลขที่29 ชั้น ม.4/8"
 
 @app.route('/api', methods=['GET'])
 def get_api():
